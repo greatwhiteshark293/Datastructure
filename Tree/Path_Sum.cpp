322d4eb105047919a6082138e5a9770c5a2afbea
greatwhiteshark293 <109818162+greatwhiteshark293@users.noreply.github.com>
2022-09-29 20:10:10 +0530

    Create Path_Sum.cpp

---
+++ Tree/Path_Sum.cpp
@@ -0,0 +1,52 @@
+112. Path Sum
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+Example 3:
+
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode* root, int targetSum,int sum){
+        if(!root) return false;
+        sum+=root->val;
+        if(!root->left && !root->right){
+            if(sum == targetSum) return true;
+        }
+        return check(root->left,targetSum,sum) || check(root->right,targetSum,sum);
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
+        int sum=0;
+       return check(root,targetSum,sum);
+        
+    }
+};
